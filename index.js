b54be141bf34d3db50f9395287b10819df9e73f1
Max Cherniavskyi <max.chernyavskyi@akqa.com>
2016-03-02 11:54:26 +0200

    Parse arguments

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 #! /usr/bin/env node
 
-console.log('fibo');
+var userArgs = process.argv.slice(2);
+
+var params = [{
+    pattern: /^--num=([\S]+)$/,
+    title: 'depth'
+}];
+
+var paramValues = userArgs.map(arg => {
+    var matched = params
+        .map(param => ({matches: arg.match(param.pattern), param: param}))
+        .filter(match => match.matches !== null);
+    return matched[0] ? {name: matched[0].param.title, value: matched[0].matches[1]} : null;
+}).filter(arg => arg !== null);
+
+console.log(paramValues);
