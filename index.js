b8c52702c6d749422acf1805340107fb69ae9ada
Max Cherniavskyi <max.chernyavskyi@akqa.com>
2016-03-02 12:36:35 +0200

    Display the numbers

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ var params = [{
     title: 'depth'
 }];
 
+// Parse arguments
+// Return [ {name: 'depth', value: 10} ]
 var paramValues = userArgs.map(arg => {
     var matched = params
         .map(param => ({matches: arg.match(param.pattern), param: param}))
@@ -14,4 +16,31 @@ var paramValues = userArgs.map(arg => {
     return matched[0] ? {name: matched[0].param.title, value: matched[0].matches[1]} : null;
 }).filter(arg => arg !== null);
 
-console.log(paramValues);
+function* fibonacci() {
+    var first = 0,
+        second = 1;
+
+    yield first; yield second;
+
+    while(true) {
+        var third = first + second;
+        yield third;
+        first = second;
+        second = third;
+    }
+}
+
+var fibo = fibonacci();
+
+var depthParam = paramValues.filter(param => param.name === 'depth').shift();
+var depth = depthParam ? depthParam.value : 10;
+
+function printFibValue(n) {
+    console.log(fibo.next().value);
+
+    if (n > 0) {
+        printFibValue(n-1);
+    }
+}
+
+printFibValue(depth);
